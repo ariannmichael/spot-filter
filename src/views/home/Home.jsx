b9afd8f844c289e6b52b7b0e572411282b9d0980
ariannmichael <chael.andrade@gmail.com>
2019-08-17 16:51:13 -0300

    Cards for genre and albums

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,36 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
 import './Home.css';
+import Genre from '../../components/genre/Genre';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userAlbums: []
+            genres: ['metalcore', 'adoracao']
         }
     }
 
-    componentDidMount() {
-        axios.get('http://localhost:8080/album/getAlbumsByGenre')
-            // .then(res => console.log(res.data.albumsByGenre));
-            .then(res => this.setState({userAlbums: res.data.albumsByGenre}));
-    
-    }
-
-    render() {        
-        // const albums = this.state.userAlbums.map(album => {
-        //     return(
-        //         <div key={album.name}>{JSON.stringify(album)}</div>
-        //     );
-        // });
-        console.log(this.state.userAlbums);
+    render() {
+        const genres = this.state.genres.map(genre => {
+            return(
+                <Genre key={genre} genre={genre} />
+            )
+        });
         
         return (
-            <div>
+            <div className="home--menu">
                 <h1 className="col-md-6">Spot Filter</h1>
                 <br/>
-                {/* {albums} */}
+                {genres}
             </div>
         );
     }
