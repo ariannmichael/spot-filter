662f2748cf8383bb961dfd26e802b00c35a7cfce
ariannmichael <chael.andrade@gmail.com>
2019-09-16 14:26:25 -0300

    callback

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,7 +17,7 @@ var scopes = ['user-library-read']
 var spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: process.env.REDIRECT_URI
+  redirectUri: process.env.REDIRECT_URI + 'callback'
 });
 
 exports.login = async function(req, res) {
