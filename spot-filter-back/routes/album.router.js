e41dd7a934d45b283f218410b39abf86b8277f09
ariannmichael <chael.andrade@gmail.com>
2019-09-16 12:51:15 -0300

    front

--- spot-filter-back/routes/album.router.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var albumController = require('../controllers/album.controller');
-
-router.get('/getAlbumsByGenre', albumController.getAlbumsByGenre);
-
-module.exports = router;
--- spot-filter-back/routes/artist.router.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var artistController = require('../controllers/artist.controller');
-
-router.get('/getArtistsByGenre', artistController.getArtistsByGenre);
-
-module.exports = router;
