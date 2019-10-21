ab97017db96479ba622eba9cbdc9f13dc9d494b3
ariannmichael <chael.andrade@gmail.com>
2019-10-21 17:52:52 -0300

    logout and login env variable

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -6,10 +6,10 @@ import axios from 'axios';
 
 export default class Login extends Component {
     componentDidMount() {
-        axios.get('http://localhost:8080/logout')
+        axios.get(process.env.REACT_APP_LOGOUT)
     }
     login = () => {
-        window.location.replace('http://localhost:8080/login')
+        window.location.replace(process.env.REACT_APP_LOGIN)
     }
 
     render() {
